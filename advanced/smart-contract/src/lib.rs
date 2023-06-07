de4dc430e597d6220191fc435c93eaa06367c52f
Kasper Dissing Bargsteen <kb@concordium.com>
2023-06-07 11:20:29 +0200

    Copy smart contract and dapp for advanced version

---
+++ advanced/smart-contract/src/lib.rs
@@ -0,0 +1,202 @@
+//! A voting smart contract example.
+//!
+//! # Description
+//! A contract that allows for conducting an election with several voting
+//! options. An `end_time` is set when the election is initialized. Only
+//! accounts are eligible to vote. Each account can change its
+//! selected voting option as often as it desires until the `end_time` is
+//! reached. No voting will be possible after the `end_time`.
+//!
+//! # Operations
+//! The contract allows for
+//!  - `initializing` the election;
+//!  - `vote` for one of the voting options;
+//!  - `view` general information about the election and the tally.
+//!
+//! # Tests
+//! The tests exist in the `./tests/tests.rs` file.
+//!
+//! Note: Vec<VotingOption> (among other variables) is an input parameter to the
+//! `init` function. Since there is a limit to the parameter size (65535 Bytes),
+//! the size of the Vec<VotingOption> is limited.
+//! https://developer.concordium.software/en/mainnet/smart-contracts/general/contract-instances.html#limits
+
+use concordium_std::{collections::BTreeMap, *};
+
+/// The human-readable description of a voting option.
+pub type VotingOption = String;
+/// The voting options are stored in a vector. The vector index is used to refer
+/// to a specific voting option.
+pub type VoteIndex = u32;
+/// Number of votes.
+pub type VoteCount = u32;
+
+/// The parameter type for the contract function `init`.
+/// Takes a description, the voting options, and the `end_time` to start the
+/// election.
+#[derive(Serialize, SchemaType)]
+pub struct InitParameter {
+    /// The description of the election.
+    pub description: String,
+    /// A vector of all voting options.
+    pub options: Vec<VotingOption>,
+    /// The last timestamp that an account can vote.
+    /// The election is open from the point in time that this smart contract is
+    /// initialized until the `end_time`.
+    pub end_time: Timestamp,
+}
+
+/// The `return_value` type of the contract function `view`.
+/// Returns a description, the `end_time`, the voting options as a vector, and
+/// the number of voting options of the current election.
+/// Also returns the tally of votes.
+#[derive(Serialize, SchemaType)]
+pub struct VotingView {
+    /// The description of the election.
+    pub description: String,
+    /// The last timestamp that an account can vote.
+    /// The election is open from the point in time that this smart contract is
+    /// initialized until the `end_time`.
+    pub end_time: Timestamp,
+    /// The map connects the index of a voting option to the number of votes
+    /// it received so far.
+    pub tally: BTreeMap<VotingOption, VoteCount>,
+}
+
+/// The contract state
+#[derive(Serialize, Clone)]
+struct State {
+    /// The description of the election.
+    description: String,
+    /// The map connects a voter to the index of the voted-for voting option.
+    ballots: BTreeMap<AccountAddress, VoteIndex>,
+    /// The last timestamp that an account can vote.
+    /// The election is open from the point in time that this smart contract is
+    /// initialized until the `end_time`.
+    end_time: Timestamp,
+    /// A vector of all voting options.
+    options: Vec<VotingOption>,
+}
+
+/// The different errors that the `vote` function can produce.
+#[derive(Reject, Serialize, PartialEq, Eq, Debug, SchemaType)]
+pub enum VotingError {
+    /// Raised when parsing the parameter failed.
+    #[from(ParseError)]
+    ParsingFailed,
+    /// Raised when the vote is placed after the election has ended.
+    VotingFinished,
+    /// Raised when voting for a voting option that does not exist.
+    InvalidVotingOption,
+    /// Raised when a smart contract tries to participate in the election. Only
+    /// accounts are allowed to vote.
+    ContractVoter,
+}
+
+/// A custom alias type for the `Result` type with the error type fixed to
+/// `VotingError`.
+pub type VotingResult<T> = Result<T, VotingError>;
+
+// Contract functions
+
+/// Initialize the contract instance and start the election.
+/// A description, the vector of all voting options, and an `end_time`
+/// have to be provided.
+#[init(contract = "voting", parameter = "InitParameter")]
+fn init<S: HasStateApi>(
+    ctx: &impl HasInitContext,
+    _state_builder: &mut StateBuilder<S>,
+) -> InitResult<State> {
+    // Parse the parameter.
+    let param: InitParameter = ctx.parameter_cursor().get()?;
+
+    // Set the state.
+    Ok(State {
+        description: param.description,
+        ballots: BTreeMap::new(),
+        end_time: param.end_time,
+        options: param.options,
+    })
+}
+
+/// Enables accounts to vote for a specific voting option. Each account can
+/// change its selected voting option with this function as often as it desires
+/// until the `end_time` is reached.
+///
+/// It rejects if:
+/// - It fails to parse the parameter.
+/// - A contract tries to vote.
+/// - It is past the `end_time`.
+#[receive(
+    contract = "voting",
+    name = "vote",
+    mutable,
+    parameter = "VotingOption",
+    error = "VotingError"
+)]
+fn vote<S: HasStateApi>(
+    ctx: &impl HasReceiveContext,
+    host: &mut impl HasHost<State, StateApiType = S>,
+) -> VotingResult<()> {
+    // Check that the election hasn't finished yet.
+    if ctx.metadata().slot_time() > host.state().end_time {
+        return Err(VotingError::VotingFinished);
+    }
+
+    // Ensure that the sender is an account.
+    let acc = match ctx.sender() {
+        Address::Account(acc) => acc,
+        Address::Contract(_) => return Err(VotingError::ContractVoter),
+    };
+
+    // Parse the parameter.
+    let new_vote: VotingOption = ctx.parameter_cursor().get()?;
+    // Find the vote index in state.options. Or return an error, if it doesn't exist.
+    let new_vote_index = match host.state().options.iter().position(|o| *o == new_vote) {
+        Some(vote_index) => vote_index as u32,
+        _ => return Err(VotingError::InvalidVotingOption),
+    };
+
+    // Insert or replace the vote for the account.
+    host.state_mut()
+        .ballots
+        .entry(acc)
+        .and_modify(|old_vote_index| *old_vote_index = new_vote_index)
+        .or_insert(new_vote_index);
+
+    Ok(())
+}
+
+/// Get the election information.
+#[receive(contract = "voting", name = "view", return_value = "VotingView")]
+fn view<S: HasStateApi>(
+    _ctx: &impl HasReceiveContext,
+    host: &impl HasHost<State, StateApiType = S>,
+) -> ReceiveResult<VotingView> {
+    // Get information from the state.
+    let description = host.state().description.clone();
+    let end_time = host.state().end_time;
+    let options = host.state().options.clone();
+    let mut tally = BTreeMap::new();
+
+    // Sum up the ballots to a tally.
+    // Looping over data that can be changed by users should be avoided in
+    // production, as there might be so many ballots that the loop cannot be
+    // processed in time.
+    for (_, vote_index) in host.state().ballots.iter() {
+        // Get the VotingOption (String).
+        let voting_option = options[*vote_index as usize].clone();
+        // Increment the existing value or insert 1.
+        tally
+            .entry(voting_option)
+            .and_modify(|current_count| *current_count += 1)
+            .or_insert(1);
+    }
+
+    // Return the election information.
+    Ok(VotingView {
+        description,
+        end_time,
+        tally,
+    })
+}
---
+++ advanced/smart-contract/tests/tests.rs
@@ -0,0 +1,249 @@
+//! This module contains integration tests for the voting contract.
+//!
+//! The best way to run these tests are with `cargo concordium test --out concordium-out/module.wasm.v1`
+//! as that will make sure to compile the module before running the tests.
+
+use concordium_smart_contract_testing::*;
+use concordium_std::Timestamp;
+use voting_contract::*;
+
+/// An account address of all 0s.
+const ACC_0: AccountAddress = AccountAddress([0; 32]);
+/// An account address of all 1s.
+const ACC_1: AccountAddress = AccountAddress([1; 32]);
+/// A `Signer` used for signing the transactions while testing.
+const SIGNER: Signer = Signer::with_one_key();
+/// The unix epoch time in milliseconds for noon at Christmas eve 2023.
+const CHRISTMAS_EVE_EPOCH: u64 = 1701873444000;
+
+/// Helper function that sets up a chain, account, and initialized contract.
+/// The contract is initialized with:
+///  - `end_time` = `CHRISTMAS_EVE_EPOCH`
+///  - `options` = ["DK", "DE", "IT"]
+fn setup_chain_and_contract(block_time: Timestamp) -> (Chain, ContractInitSuccess) {
+    // Setup the test chain struct.
+    let mut chain = Chain::new_with_time(block_time);
+
+    // Create two accounts.
+    chain.create_account(Account::new(ACC_0, Amount::from_ccd(10000)));
+    chain.create_account(Account::new(ACC_1, Amount::from_ccd(10000)));
+
+    // Load the module.
+    let module =
+        module_load_v1("./concordium-out/module.wasm.v1").expect("Module file should exist");
+
+    // Deploy the module.
+    let deployment = chain
+        .module_deploy_v1(SIGNER, ACC_0, module)
+        .expect("Deploying valid module should succeed");
+
+    // Initialize the contract.
+    let initialization = chain
+        .contract_init(
+            SIGNER,
+            ACC_0,
+            Energy::from(10000),
+            InitContractPayload {
+                amount: Amount::zero(),
+                mod_ref: deployment.module_reference,
+                init_name: OwnedContractName::new_unchecked(String::from("init_voting")),
+                param: OwnedParameter::from_serial(&InitParameter {
+                    description: String::from("Concordium EuroVision"),
+                    options: vec![String::from("DK"), String::from("DE"), String::from("IT")],
+                    end_time: Timestamp::from_timestamp_millis(CHRISTMAS_EVE_EPOCH), // Noon on Christmas eve.
+                })
+                .expect("Valid parameter size"),
+            },
+        )
+        .expect("Initialization should succeed");
+
+    (chain, initialization)
+}
+
+/// Test that an account cannot vote if it is past the `end_time` of the election.
+#[test]
+fn test_vote_after_end_time() {
+    // Set up the chain with a block time later than `CHRISTMAS_EVE_EPOCH`, such voting is no longer permitted.
+    let (mut chain, initialization) =
+        setup_chain_and_contract(Timestamp::from_timestamp_millis(CHRISTMAS_EVE_EPOCH + 1));
+
+    // Try to vote
+    let update = chain
+        .contract_update(
+            SIGNER,
+            ACC_0,
+            Address::Account(ACC_0),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: initialization.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked(String::from("voting.vote")),
+                message: OwnedParameter::from_serial(&VotingOption::from("DE"))
+                    .expect("Parameter has valid length"),
+            },
+        )
+        .expect_err("Vote fails");
+    // Get the return value by unwrapping the `Option`.
+    let return_value = update.return_value().expect("A value should be returned");
+    // Deserialize it into a `VotingError`.
+    let error: VotingError =
+        from_bytes(&return_value).expect("Return value should be a VotingError.");
+    // Check that it failed for the right reason.
+    assert_eq!(error, VotingError::VotingFinished);
+}
+
+/// Test that voting on an unknown option fails.
+#[test]
+fn test_vote_on_unknown_option_fails() {
+    // Set up the chain with a block time below the end time.
+    let (mut chain, initialization) = setup_chain_and_contract(Timestamp::from_timestamp_millis(0));
+
+    // Try to vote on an invalid option.
+    let update = chain
+        .contract_update(
+            SIGNER,
+            ACC_0,
+            Address::Account(ACC_0),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: initialization.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked(String::from("voting.vote")),
+                message: OwnedParameter::from_serial(&VotingOption::from("IN")) // India is a valid option.
+                    .expect("Parameter has valid length"),
+            },
+        )
+        .expect_err("Vote fails");
+    // Get the return value by unwrapping the `Option`.
+    let return_value = update.return_value().expect("A value should be returned");
+    // Deserialize it into a `VotingError`.
+    let error: VotingError =
+        from_bytes(&return_value).expect("Return value should be a VotingError.");
+    // Check that it failed for the right reason.
+    assert_eq!(error, VotingError::InvalidVotingOption);
+}
+
+/// Test that voting works.
+/// - This checks that voting with a valid option is stored correctly,
+/// - That you can change your vote,
+/// - And that votes by multiple accounts are stored correctly.
+///
+/// The test works by alternating between using `chain.contract_update` for updating the contract when voting,
+/// and using `chain.contract_invoke` when invoking the view function.
+#[test]
+fn test_valid_voting_with_multiple_accounts() {
+    // Set up the chain with a block time below the end time.
+    let (mut chain, initialization) = setup_chain_and_contract(Timestamp::from_timestamp_millis(0));
+
+    // ACC_0 votes on Germany.
+    chain
+        .contract_update(
+            SIGNER,
+            ACC_0,
+            Address::Account(ACC_0), // ACC_0 is the sender.
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: initialization.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked(String::from("voting.vote")),
+                message: OwnedParameter::from_serial(&VotingOption::from("DE")) // Voting on Germany.
+                    .expect("Parameter has valid length"),
+            },
+        )
+        .expect("Voting succeeds");
+
+    // Use `contract_invoke` to get the `VotingView`.
+    let view_0 = chain
+        .contract_invoke(
+            ACC_0,
+            Address::Account(ACC_0),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: initialization.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked(String::from("voting.view")),
+                message: OwnedParameter::empty(),
+            },
+        )
+        .expect("Invoke succeeds.");
+    let voting_view_0: VotingView =
+        from_bytes(&view_0.return_value).expect("Return value should be a VotingView");
+    // There is only a single entry.
+    assert_eq!(voting_view_0.tally.len(), 1);
+    // There is one vote on Germany.
+    assert_eq!(voting_view_0.tally.get("DE"), Some(&1));
+
+    // ACC_1 votes on Denmark.
+    chain
+        .contract_update(
+            SIGNER,
+            ACC_1,
+            Address::Account(ACC_1), // ACC_1 is now the sender.
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: initialization.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked(String::from("voting.vote")),
+                message: OwnedParameter::from_serial(&VotingOption::from("DK")) // Voting on Denmark.
+                    .expect("Parameter has valid length"),
+            },
+        )
+        .expect("Voting succeeds");
+    let view_1 = chain
+        .contract_invoke(
+            ACC_1,
+            Address::Account(ACC_1), // The account used here doesn't matter, as it is just an invoke, not an update.
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: initialization.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked(String::from("voting.view")),
+                message: OwnedParameter::empty(),
+            },
+        )
+        .expect("Invoke succeeds.");
+    let voting_view_1: VotingView =
+        from_bytes(&view_1.return_value).expect("Return value should be a VotingView");
+    // There are now two entries.
+    assert_eq!(voting_view_1.tally.len(), 2);
+    // There is one vote on Germany.
+    assert_eq!(voting_view_1.tally.get("DE"), Some(&1));
+    // .. And one vote on Denmark.
+    assert_eq!(voting_view_1.tally.get("DK"), Some(&1));
+
+    // ACC_0 changes votes to Denmark.
+    chain
+        .contract_update(
+            SIGNER,
+            ACC_0,
+            Address::Account(ACC_0), // ACC_0 is the sender.
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: initialization.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked(String::from("voting.vote")),
+                message: OwnedParameter::from_serial(&VotingOption::from("DK")) // Changing vote to Denmark.
+                    .expect("Parameter has valid length"),
+            },
+        )
+        .expect("Voting succeeds");
+    let view_2 = chain
+        .contract_invoke(
+            ACC_1,
+            Address::Account(ACC_1), // The account used here doesn't matter, as it is just an invoke, not an update.
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: initialization.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked(String::from("voting.view")),
+                message: OwnedParameter::empty(),
+            },
+        )
+        .expect("Invoke succeeds.");
+    let voting_view_2: VotingView =
+        from_bytes(&view_2.return_value).expect("Return value should be a VotingView");
+    // There is only one entry again.
+    assert_eq!(voting_view_2.tally.len(), 1);
+    // There are two votes on Denmark.
+    assert_eq!(voting_view_2.tally.get("DK"), Some(&2));
+}
